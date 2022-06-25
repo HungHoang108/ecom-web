bc9f36dfc88263823c2dfdcf41afe7c150a17334
hung hoang <hung.xamk@gmail.com>
2022-06-26 00:49:34 +0300

    adding functions to add-to-cart button which after clicking the clicked item will appear on shopping cart

---
+++ crwn-clothing-lesson-6/src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,12 @@
+import './cart-item.styles.scss'
+
+export const CartItem =({cartItem})=>{
+    const {name, quantity}= cartItem
+
+    return (
+        <div>
+            <h2>{name}</h2>
+            <span>{quantity}</span>
+        </div>
+    )
+}
--- crwn-clothing-lesson-6/src/contexts/cart.context.jsx
+++ crwn-clothing-lesson-6/src/contexts/cart.context.jsx
@@ -1,10 +1,34 @@
 import { createContext, useState } from "react";
 
-export const CartContext = createContext();
+const addCartItem = (cartItems, productToAdd)=>{
+    // find if cartItems contains productToAdd
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === productToAdd.id)
+
+    //if found, increase quantity by 1
+    if (existingCartItem) {
+        return cartItems.map(cartItem => cartItem.id === productToAdd.id?
+            {...cartItem, quantity: cartItem.quantity + 1} :
+            cartItem)
+    }
+
+    return [...cartItems, {...productToAdd, quantity: 1}]
+}
+
+export const CartContext = createContext({
+    isCartOpen: false,
+    setIsCartOpen: ()=>{},
+    cartItems: [],
+    addItemToCart: ()=> {}
+});
 
 export const CartProvider = ({children})=>{
     const [isCartOpen, setIsCartOpen] = useState(false)
-    const value = {isCartOpen, setIsCartOpen}
+    const [cartItems, setCartItems] = useState([])
+
+    const addItemToCart = (productToAdd)=>{
+        setCartItems(addCartItem(cartItems, productToAdd))
+    }
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems}
     
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
